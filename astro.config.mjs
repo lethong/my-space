29193e41fde2678eb9b6a2a6199decc3ab54438c
lethong <bathongle@gmail.com>
2026-09-22 00:00:55 +0700

    Add site and base configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://lethong.github.io',
+  base: '/my-space',
   integrations: [
     starlight({
       title: 'Tài liệu của Thông',
