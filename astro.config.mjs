a23be57f65e5483a30e626af059525b74f78b680
lethong <bathongle@gmail.com>
2026-09-24 10:08:21 +0700

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'https://lethong.github.io', 
-  base: '/my-space', 
+  site: 'https://myspace.lebathong.com', 
+  base: '', 
   integrations: [
     starlight({
       title: 'Tài liệu của Thông',
